b3753f7f1248d8ae4fad93c0a81f50d29d0970ef
Mukesh101-lab <mukeshpatel314894@gmail.com>
2026-05-09 21:16:32 +0530

    update repo

--- Operators/Qustion.java
+++ Operators/Qustion.java
@@ -9,15 +9,39 @@ public class Qustion {
         // System.out.println(x * (y / x));
 
         //assignment qustion 2
-        int x = 200 , y = 50 , z = 100;
-        if(x > y && y > z){
-            System.out.println("hello");
-        }
-        if(z > y && z < x){
-            System.out.println("java");
-        }
-        if((y + 200) < x && (y + 150) < z){
-            System.out.println("hello java");
-        }
+        // int x = 200 , y = 50 , z = 100;
+        // if(x > y && y > z){
+        //     System.out.println("hello");
+        // }
+        // if(z > y && z < x){
+        //     System.out.println("java");
+        // }
+        // if((y + 200) < x && (y + 150) < z){
+        //     System.out.println("hello java");
+        // }
+
+        //assignment qustions 3
+        // int x , y , z ;
+        // x = y = z = 2;
+        // x += y;
+        // y -= z;
+        // z /= (x + y);
+        // System.out.println(x);
+        // System.out.println(y);
+        // System.out.println(z);
+
+        //assignment qustion 4
+        // int x = 9, y = 12;
+        // int a  =2 , b = 4, c = 6;
+        // int exp = 4/3 * (x +34) + 9 * (a + b * c) + (3 + y *(2 + a)) / (a + b * y);
+        // System.out.println(exp);
+
+        //assignment qustion 5
+        // int x = 10 , y = 5;
+        // int exp1 = (y * (x / y + x / y));
+        // int exp2 = (y * x / y + y *x / y);
+        // System.out.println(exp1);
+        // System.out.println(exp2);
+
     }
 }
