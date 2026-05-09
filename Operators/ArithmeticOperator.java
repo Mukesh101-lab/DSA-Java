03c03ff1731352e734e0eac5fece6fccecaac750
Mukesh101-lab <mukeshpatel314894@gmail.com>
2026-05-09 20:48:26 +0530

    update repo

---
+++ Operators/ArithmeticOperator.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("add " + (a + b));
+        System.out.println("multipication " + (a * b));
+        System.out.println("subtract " + (a-b));
+        System.out.println("divisipon " + (a / b));
+        System.out.println("modulo " + (a % b));
+    }
+}
---
+++ Operators/Qustion.java
@@ -0,0 +1,23 @@
+package Operators;
+
+public class Qustion {
+    public static void main(String[] args) {
+        //assignment qustion 1
+        // int x = 2 ; 
+        // int  y = 5 ;
+        // System.out.println(x * y / x);
+        // System.out.println(x * (y / x));
+
+        //assignment qustion 2
+        int x = 200 , y = 50 , z = 100;
+        if(x > y && y > z){
+            System.out.println("hello");
+        }
+        if(z > y && z < x){
+            System.out.println("java");
+        }
+        if((y + 200) < x && (y + 150) < z){
+            System.out.println("hello java");
+        }
+    }
+}
