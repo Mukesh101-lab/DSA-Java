3af6217aa2aee18186afc12e73c3f88ed0d1a9bd
Mukesh101-lab <mukeshpatel314894@gmail.com>
2026-05-10 13:19:38 +0530

    update repo

---
+++ ConditionalStatement/AssignmentQustion/Qustion.java
@@ -0,0 +1,66 @@
+package ConditionalStatement.AssignmentQustion;
+
+import java.util.Scanner;
+
+public class Qustion {
+    public static void main(String[] args) {
+        //Assignment Qustion 1
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+
+        // if(n > 0){
+        //     System.out.println("Positive Number");
+        // }else{
+        //     System.out.println("Negative Nuber");
+        // }
+
+        //Assignment Qustion 2
+        // double temp = 103.5;
+        // if(temp > 100){
+        //     System.out.println("fever");
+        // }else{
+        //     System.out.println("Not Fever");
+        // }
+
+        //Assignment Qustion 3
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // switch (n) {
+        //     case 1:
+        //         System.out.println("monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("friday");
+        //         break;
+        //     case 6:
+        //         System.out.println("saturday");
+        //         break;
+        //     case 7:
+        //         System.out.println("sunday");
+        //         break;
+            
+        //     default:
+        //         System.out.println("default day");
+        //         break;
+        // }
+
+        //Assignment Qustion4
+        // int a = 63 , b = 36;
+        // boolean x = ( a < b ) ? true : false;
+        // System.out.println(x);
+        // int y = (a > b) ? a : b ;
+        // System.out.println(y);
+
+        //Assignment Qustion 5
+
+    }
+}
