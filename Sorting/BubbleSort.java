2d60aa9db123d2f781b517b4a4cf2e3435eb6877
Mukesh101-lab <mukeshpatel314894@gmail.com>
2026-06-29 20:43:37 +0530

    update repo

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,31 +1,25 @@
 package Sorting;
 
-public class BubbleSort {
-    public static void BubbleSort(int arr[]){
+public class bubbleSort {
+    public static void sortArray(int arr[]){
         int n = arr.length;
         for(int i=0;i<n-1;i++){
-            boolean swap = false;
             for(int j=0;j<n-1-i;j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
-                    swap = true;
                 }
             }
-            if(swap == false){
-                break;
-            }
-        }
-    }
-    public static void printBubbleSort(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
         }
     }
     public static void main(String[] args) {
         int arr[] = {5,4,1,3,2};
-        BubbleSort(arr);
-        printBubbleSort(arr);
+        sortArray(arr);
+
+        System.out.print("Sort Array : ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
