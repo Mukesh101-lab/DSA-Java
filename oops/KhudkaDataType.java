702abfc96b8112be6bdfb70d3c84d8136223a6f3
Mukesh101-lab <mukeshpatel314894@gmail.com>
2026-08-08 07:15:07 +0530

    update repo

--- oops/KhudkaDataType.java
+++ oops/KhudkaDataType.java
@@ -1,10 +1,19 @@
 package oops;
 
+
 public class KhudkaDataType {
     public static class Student {
         String name;
         int rno;
         double cgpa;
+
+        void print(){
+            System.out.println(name + " "+ rno +" "+cgpa);
+        }
+    }
+
+    public static void Change(Student s){
+        s.rno = 90;
     }
     public static void main(String[] args) {
         Student s = new Student();
@@ -12,6 +21,13 @@ public class KhudkaDataType {
         s.rno = 91;
         s.cgpa = 8.29;
 
-        System.out.println(s.name + " "+ s.cgpa + " " + s.rno);
+        s.print();
+
+        // System.out.println(s.name + " "+ s.cgpa + " " + s.rno);
+
+        // Change(s);
+
+        // System.out.println(s.name + " "+ s.cgpa + " " + s.rno);
+
     }
 }
