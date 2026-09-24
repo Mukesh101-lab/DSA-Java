dafa93fed00a35e6d0b6d461b8bd463b2eb0d2cd
Mukesh101-lab <mukeshpatel314894@gmail.com>
2026-09-24 16:44:22 +0530

    update repo

---
+++ Arrays/basic.java
@@ -0,0 +1,9 @@
+// package Arrays;
+
+public class basic {
+    public static void main(String[] args) {
+        int[] x = {6,19,7,69,9,171,5};
+
+        System.out.println(x[x.length-5]);
+    }
+}
